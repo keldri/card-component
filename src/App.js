6db224b34879484fec806f1b6bfeb6579ba47514
Kate Eldridge <kathryneeldridge@gmail.com>
2018-01-28 12:27:05 -0500

    Add markup for card component and form

--- src/App.js
+++ src/App.js
@@ -1,21 +1,34 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import AddBusinessCardForm from './Components/AddBusinessCardForm';
+import BusinessCard from './Components/BusinessCard';
+import './styles/styles.css';
+
+
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+      <div className='app'>
+        <header>
+            <div className='component__title'>
+              <h1>Card Component</h1>
+            </div>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className="component__wrapper">
+          <div className='container'>
+            <BusinessCard/>
+            <BusinessCard/>
+          </div>
+        </div>
+        <div className="component__form">
+          <div className='container'>
+            <AddBusinessCardForm/>
+          </div>
+        </div>
       </div>
     );
   }
 }
-
 export default App;
