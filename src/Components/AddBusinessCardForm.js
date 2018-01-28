3285c46fb5451be724ae7be1a2ca0bc53108f2f1
Kate Eldridge <kathryneeldridge@gmail.com>
2018-01-28 15:06:39 -0500

    Update BusinessCard to use props
    - replacing hardcoded data with props that currently pull from object in index.js.
    - The object in index.js will be removed once data can be retrieved from firebase

--- src/Components/AddBusinessCardForm.js
+++ src/Components/AddBusinessCardForm.js
@@ -5,17 +5,46 @@ class AddBusinessCardForm extends Component {
   render() {
     return(
       <div className='container'>
+        <div className='component__title'>
+          <h1>Add Card</h1>
+        </div>
         <section className='add-item'>
           <form>
-            <input type="text" name="ctaUrl" placeholder="ctaUrl?" />
-            <input type="text" name="ctaText" placeholder="ctaTextg?" />
-            <input type="text" name="logoUrl" placeholder="logoUrl?" />
-            <input type="text" name="promoMessage" placeholder="promoMessage?" />
-            <input type="text" name="promoCode" placeholder="promoCode?" />
-            <input type="text" name="features" placeholder="also this is an object?" />
-            <input type="text" name="name" placeholder="name?" />
-            <input type="text" name="profileHref" placeholder="profileHref?" />
-          <button>Add Item</button>
+          <fieldset>
+            <field>
+              <label for="ctaUrl">Add CTA URL</label>
+              <input type="text" name="ctaUrl" id="ctaUrl"/>
+            </field>
+            <field>
+              <label for="ctaText">Add CTA Text</label>
+              <input type="text" name="ctaText" id="ctaText"/>
+            </field>
+            <field>
+              <label for="logoUrl">Add Logo URL</label>
+              <input type="text" name="logoUrl" id="logoUrl"/>
+            </field>
+            <field>
+              <label for="promoMessage">Add Promo Message</label>
+              <input type="text" name="promoMessage" id="promoMessage"/>
+            </field>
+            <field>
+              <label for="promoCode">Add Promo Code</label>
+              <input type="text" name="promoCode" id="promoCode"/>
+            </field>
+            <field>
+              <label for="features">Add Features</label>
+              <input type="text" name="features" id="features"/>
+            </field>
+            <field>
+              <label for="name">Add Company Name</label>
+              <input type="text" name="name" id="name"/>
+            </field>
+            <field>
+              <label for="name">Add Company Profile URL</label>
+              <input type="text" name="name" id="name"/>
+            </field>
+            </fieldset>
+            <button className="button--primary">Add Busines Card</button>
           </form>
         </section>
         <section className='display-item'>
