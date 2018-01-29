2dc53f5cef6b4bd32285fcebf73f0b243b7f927f
Kate Eldridge <kathryneeldridge@gmail.com>
2018-01-29 05:57:40 -0500

    Add app methods
    - handleCheck() for checkboxes
    - handleChange() for text inputs
    - handleSubmit() to submit form and create component
    - componentDidMount()
    - openModal
    - closeModal

--- src/App.js
+++ src/App.js
@@ -27,6 +27,93 @@ class App extends Component {
       isModalOpen: false
     }
 
+    this.handleChange = this.handleChange.bind(this);
+    this.handleCheck = this.handleCheck.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.openModal = this.openModal.bind(this);
+    this.closeModal = this.closeModal.bind(this);
+  }
+
+  handleCheck(e) {
+    const newSelection = e.target.value;
+    let newSelectionArray;
+
+    if(this.state.cardFeatures.indexOf(newSelection) > -1) {
+      newSelectionArray = this.state.cardFeatures.filter(i => i !== newSelection)
+    } else {
+      newSelectionArray = [...this.state.cardFeatures, newSelection];
+    }
+    this.setState({ cardFeatures: newSelectionArray });
+  }
+
+  handleChange(e) {
+    this.setState({
+      [e.target.name]: e.target.value
+    });
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    const itemsRef = firebase.database().ref('items');
+    const item = {
+      ctaUrl: this.state.ctaUrl,
+      ctaText: this.state.ctaText,
+      logoUrl: this.state.logoUrl,
+      promoMessage: this.state.promoMessage,
+      promoCode: this.state.promoCode,
+      name: this.state.name,
+      profileHref: this.state.profileHref,
+      features: this.state.features,
+      cardFeatures: this.state.cardFeatures,
+    }
+    itemsRef.push(item);
+    this.setState({
+      ctaUrl: '',
+      ctaText: '',
+      logoUrl: '',
+      promoMessage: '',
+      promoCode: '',
+      name: '',
+      profileHref: '',
+      cardFeatures: []
+    });
+
+    console.log(this.state.cardFeatures)
+    this.closeModal();
+    e.target.reset();
+  }
+
+  componentDidMount() {
+    const itemsRef = firebase.database().ref('items');
+    itemsRef.on('value', (snapshot) => {
+      let items = snapshot.val();
+      let newState = [];
+      for (let item in items) {
+        newState.push({
+          id: item,
+          ctaUrl: items[item].ctaUrl,
+          ctaText: items[item].ctaText,
+          logoUrl: items[item].logoUrl,
+          promoMessage: items[item].promoMessage,
+          promoCode: items[item].promoCode,
+          name: items[item].name,
+          profileHref: items[item].profileHref,
+          cardFeatures: items[item].cardFeatures
+        });
+      }
+      this.setState({
+        items: newState
+      });
+    });
+  }
+
+  openModal() {
+    this.setState({ isModalOpen: true })
+  }
+
+  closeModal() {
+    this.setState({ isModalOpen: false })
+  }
 
 class App extends Component {
   render() {
