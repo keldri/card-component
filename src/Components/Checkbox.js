ef65c25ef8e669422513d58c642acc864461b51e
Kate Eldridge <kathryneeldridge@gmail.com>
2018-01-29 06:07:32 -0500

    Add checkbox component for features

---
+++ src/Components/Checkbox.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Checkbox extends Component {
+  render() {
+    return(
+		<div className="custom-input custom-input--checkbox">
+		  <input type="checkbox" name={this.props.feature} id={this.props.feature} value={this.props.feature} checked={this.props.checked} onChange={this.props.handleCheck}/>
+		  <label htmlFor={this.props.feature}>{this.props.feature}</label>
+		</div>
+    );
+  }
+}
+
+export default Checkbox;
